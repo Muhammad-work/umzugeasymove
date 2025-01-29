2b16f11fc93448f819a81130ec593a980e7da4bc
Muhammad-work <work47687@gmail.com>
2025-01-29 12:16:38 +0500

    add same changes

--- 123Umzugshelfer/resources/views/front/move_company.blade.php
+++ 123Umzugshelfer/resources/views/front/move_company.blade.php
@@ -9,24 +9,38 @@
 
    {{-- hero section --}}
 
-    <section class="w-full h-auto bg-white py-6">
-        <div class="w-full h-full flex justify-center place-items-center">
-            <div class="w-[40%] flex flex-col gap-3 ">
-                <h1 class="text-4xl font-bold text-center">Umzugsunternehmen <br> Berlin</h1>
-                <div class="flex gap-2 place-items-center">
-                    <input type="text" name="" id="" placeholder="Postlezahi eingeben" class="p-2 outline-none rounded border-2 border-[#ccc]">
-                    <a href="{{ route('calculater') }}" class="bg-[#06BF8B] text-white px-2 py-3 rounded uppercase ">Jetzt angebot erhalten</a>
-                </div>
-                <p class="font-medium"><i class="fa-solid fa-check bg-[#06BF8B] p-2 text-white rounded-full"></i> No subcontractors</p>
-                <p class="font-medium"><i class="fa-solid fa-check bg-[#06BF8B] p-2 text-white rounded-full"></i> 100% Own Employees</p>
-                <p class="font-medium"><i class="fa-solid fa-check bg-[#06BF8B] p-2 text-white rounded-full"></i> Fixed price bookable</p>
-                <p class="font-medium"><i class="fa-solid fa-check bg-[#06BF8B] p-2 text-white rounded-full"></i> Regionally near you!</p>
+   <section class="w-full h-auto bg-white py-6">
+    <div class="w-full h-full flex flex-col md:flex-row justify-center items-center gap-6 px-4">
+        <div class="lg:w-[40%] w-full flex flex-col gap-3 text-center md:text-left">
+            <h1 class="text-3xl md:text-4xl font-bold">Umzugsunternehmen <br> Berlin</h1>
+            <div class="flex flex-col md:flex-row gap-2 items-center">
+                <input type="text" placeholder="Postleitzahl eingeben"
+                       class="p-2 w-full md:w-auto outline-none rounded border-2 border-gray-300">
+                <a href="{{ route('calculater') }}"
+                   class="bg-[#06BF8B] text-white px-4 py-3 rounded uppercase text-center w-full md:w-auto">
+                   Jetzt angebot erhalten
+                </a>
             </div>
-            <div class="w-[40%] h-full">
-                <img class="w-full " src="{{ asset('storage/img/hero.svg') }}" alt="">
+            <div class="flex flex-col items-center md:items-start gap-2">
+                <p class="font-medium flex items-center gap-2">
+                    <i class="fa-solid fa-check bg-[#06BF8B] p-2 text-white rounded-full"></i> No subcontractors
+                </p>
+                <p class="font-medium flex items-center gap-2">
+                    <i class="fa-solid fa-check bg-[#06BF8B] p-2 text-white rounded-full"></i> 100% Own Employees
+                </p>
+                <p class="font-medium flex items-center gap-2">
+                    <i class="fa-solid fa-check bg-[#06BF8B] p-2 text-white rounded-full"></i> Fixed price bookable
+                </p>
+                <p class="font-medium flex items-center gap-2">
+                    <i class="fa-solid fa-check bg-[#06BF8B] p-2 text-white rounded-full"></i> Regionally near you!
+                </p>
             </div>
         </div>
-    </section>
+        <div class="lg:w-[40%] w-full">
+            <img class="w-full max-w-full" src="{{ asset('storage/img/hero.svg') }}" alt="Umzugsunternehmen Berlin">
+        </div>
+    </div>
+   </section>
 
    {{-- hero section --}}
 
